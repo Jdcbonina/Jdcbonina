5ef26057177f43ebcdbc7ed8593be829e4fd61db
Joey Bonina <jdcbonina@gmail.com>
2025-06-19 19:29:19 +0800

    Update timeline.tsx

--- components/ui/timeline.tsx
+++ components/ui/timeline.tsx
@@ -34,21 +34,28 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
 
   return (
     <div className="w-full font-sans" ref={containerRef}>
-      <div className="max-w-7xl mx-auto py-20 px-4 md:px-8 lg:px-10">
-        <h2 className="text-lg md:text-4xl mb-4 text-black dark:text-white max-w-4xl">
-          My Professional Journey
-        </h2>
-        <p className="text-neutral-700 dark:text-neutral-300 text-sm md:text-base max-w-sm">
-          I&apos;ve been working as a React Junior Software Developer for the
-          past 2 years. Here&apos;s a look at the milestones and progress
-          I&apos;ve made along the way.
-        </p>
-      </div>
+      <motion.div
+          initial={{ opacity: 0.0, x: -50 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.8, delay: 0.2, ease: "easeInOut", }}
+        className="max-w-7xl mx-auto py-20 px-4 md:px-8 lg:px-10">
+          <h2 className="text-lg md:text-4xl mb-4 text-black dark:text-white max-w-4xl">
+            My Professional Journey
+          </h2>
+          <p className="text-neutral-700 dark:text-neutral-300 text-sm md:text-base max-w-sm">
+            I&apos;ve been working as a React Junior Software Developer for the
+            past 2 years. Here&apos;s a look at the milestones and progress
+            I&apos;ve made along the way.
+          </p>
+      </motion.div>
 
       <div ref={ref} className="relative max-w-7xl mx-auto pb-20">
         {data.map((item, index) => (
-          <div
+          <motion.div
             key={index}
+            initial={{ opacity: 0.0, y: 100 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.4, delay: index * 0.2, ease: "easeInOut", }}
             className="flex justify-start pt-10 md:pt-40 md:gap-10"
           >
             <div className="sticky flex flex-col md:flex-row z-40 items-center top-40 self-start max-w-xs lg:max-w-sm md:w-full">
@@ -66,7 +73,7 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
               </h3>
               {item.content}{" "}
             </div>
-          </div>
+          </motion.div>
         ))}
         <div
           style={{
